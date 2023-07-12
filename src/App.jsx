69e60ae91b1e99465743f26f6e32ffdddd3f31e3
therohanmehta <rohandhn786@gmail.com>
2023-07-12 22:32:07 +0530

    useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,55 @@
+import { useReducer, useState } from "react";
+function reducer(state, action) {
+  switch (action.type) {
+    case "inc":
+      return { initialState: state.initialState + 1 };
 
+    case "dec":
+      return { initialState: state.initialState - 1 };
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  )
+    case "incByValue":
+      return { initialState: state.initialState + action.payload };
+
+    default:
+      return state.initialState;
+  }
 }
 
-export default App
+function App() {
+  const [num, setNum] = useState();
+  const [state, dispatch] = useReducer(reducer, { initialState: 0 });
+  return (
+    <div>
+      <h1>{state.initialState}</h1>
+      <input
+        type="number"
+        onChange={(e) => {
+          setNum(+e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          dispatch({ type: "incByValue", payload: num });
+        }}
+      >
+        increase By Number
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: "inc" });
+        }}
+      >
+        increase
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: "dec" });
+        }}
+      >
+        decrease
+      </button>
+    </div>
+  );
+}
+
+export default App;
