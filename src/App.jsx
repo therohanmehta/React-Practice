ab7f215ee4d7e24078c02c0d105562ac0402dff4
therohanmehta <rohandhn786@gmail.com>
2023-07-13 01:04:08 +0530

    redux/toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,42 @@
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import { decrement, incByValue, increment } from "./reduxSlicer";
 
-import { useDispatch, useSelector } from 'react-redux'
-import './App.css'
-import { decrement, incByValue, increment } from './reduxSlicer'
-
-import { useState } from 'react'
-
+import { useState } from "react";
 
 function App() {
-const count=useSelector((state)=>(state.counter.value))
-const dispatch=useDispatch()
-const [num,setNum]=useState()
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+  const [num, setNum] = useState();
+
   return (
     <>
-  
-     <h2>{count}</h2>
-     <button onClick={()=>{dispatch(increment())}}>inc</button>
-     <button >dec</button>
-     <input type="text" onChange={(e)=>{setNum(+e.target.value)}}/>
-     <button onClick={()=>{dispatch(incByValue(num))}}>incbyValue</button>
-
+      <h2>{count}</h2>
+      <button
+        onClick={() => {
+          dispatch(increment());
+        }}
+      >
+        inc
+      </button>
+      <button   onClick={() => {
+          dispatch(decrement());
+        }}>dec</button>
+      <input
+        type="text"
+        onChange={(e) => {
+          setNum(+e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          dispatch(incByValue(num));
+        }}
+      >
+        incbyValue
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
