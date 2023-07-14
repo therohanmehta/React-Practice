ff6b5497484787b4ad89f58acb9e5e6cb7af19b0
therohanmehta <rohandhn786@gmail.com>
2023-07-15 01:44:51 +0530

    usereducer practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import { useReducer } from 'react'
-import './App.css'
+import { useReducer } from "react";
+import "./App.css";
+function reducer(state, action) {
+  if (action.type === "inc") {
+    // return { value: state.value + 1 };
+    console.log(123)
+  } 
+  else if (action.type === "dec") {
+    return { value: state.value - 1 };
+  }
+}
 
 function App() {
- const [rohan,dispatch]=useReducer(reducer,{value:2})
+  const [state, dispatch] = useReducer(reducer, { value: 2 });
 
   return (
     <>
-     <h1>hello </h1>
+      <h1>hello {state.value}</h1>
+      <button onClick={()=>{dispatch(type=inc)}}>Inc</button>
+      <button>Dec</button>
+      <button>inc by value</button>
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
