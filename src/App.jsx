e22c680bc907fc9354b967560bc2d2b8d0f7dda6
therohanmehta <rohandhn786@gmail.com>
2023-07-15 01:50:15 +0530

    usereducer practice

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,41 @@ import { useReducer } from "react";
 import "./App.css";
 function reducer(state, action) {
   if (action.type === "inc") {
-    // return { value: state.value + 1 };
-    console.log(123)
-  } 
-  else if (action.type === "dec") {
+    return { value: state.value + 1 };
+  } else if (action.type === "dec") {
     return { value: state.value - 1 };
+  } else if (action.type === "incByValue") {
+    return { value: state.value +action.payload };
   }
 }
-
 function App() {
   const [state, dispatch] = useReducer(reducer, { value: 2 });
-
+  const myNum = 10;
   return (
     <>
       <h1>hello {state.value}</h1>
-      <button onClick={()=>{dispatch(type=inc)}}>Inc</button>
-      <button>Dec</button>
-      <button>inc by value</button>
 
+      <button
+        onClick={() => {
+          dispatch({ type: "inc" });
+        }}
+      >
+        Inc
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: "dec" });
+        }}
+      >
+        Dec
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: "incByValue",payload:myNum });
+        }}
+      >
+        inc by value
+      </button>
     </>
   );
 }
